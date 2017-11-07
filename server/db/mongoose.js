eabed80b32f74d3a192b8f9a28a39bbcf9375a3c
Danilo Sanchez <df.sanchez@outlook.com>
2017-11-07 14:45:37 +0100

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
+mongoose.connect('mongodb://danilo:Rick1989@ds151355.mlab.com:51355/todo-app-api' || 'mongodb://localhost:27017/TodoApp', {useMongoClient: true});
 
 module.exports = {mongoose};
